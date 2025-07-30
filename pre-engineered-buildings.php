7bd9ccf70b087edebf612b1c4b1ddba9b7bba288
unknown <rbabu310197@gmail.com>
2025-07-30 22:12:14 +0530

    start to create single service page

---
+++ pre-engineered-buildings.php
@@ -0,0 +1,303 @@
+<?php include 'components/header.php' ?>
+<!--header end -->
+<!--Breadcrumb start-->
+<div class="ed_pagetitle">
+	<div class="ed_img_overlay"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12">
+				<div class="page_title">
+					<h2>Pre Engineered Building</h2>
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12">
+				<ul class="breadcrumb">
+					<li><a href="index.html">home</a></li>
+					<li>//</li>
+					<li><a href="services.html">services</a></li>
+					<li>//</li>
+					<li><a href="services_single_rs.html">Pre Engineered Building</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Breadcrumb end-->
+<!--Blog content start-->
+<div class="ed_transprentbg ed_toppadder90">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-9 col-md-12 col-sm-12">
+				<div class="ed_blog_all_item">
+					<div class="ed_blog_item ed_bottompadder50">
+						<div class="ed_blog_image">
+							<img src="assets/images/content/price_bg.jpg" alt="blog image" />
+						</div>
+						<div class="ed_blog_info">
+							<h2>Pre Engineered Building</h2>
+							<!-- <ul>
+								<li><a href="#"><i class="fa fa-user"></i> james marco</a></li>
+								<li><a href="#"><i class="fa fa-clock-o"></i> March 22, 2022</a></li>
+								<li><a href="#"><i class="fa fa-comment-o"></i> 4 comments</a></li>
+							</ul> -->
+							<p>Pre Engineered Building are custom made to suit a wide range of application in different
+								industry sectors. It largely depends upon the requirement of the customers. We are
+								India's No.1 Pre-Engineered Building Manufacturers. These metal buildings are different
+								from conventional buildings in ways like speed in delivery and erection, flexibility in
+								expansion and withstand severe weather conditions. We are a one stop solution provider
+								for all your structural needs and have products ranging from Porta cabins, Canopies,
+								Mezzanine Floors, Stairs, etc. Having an experience of over two decades in this industry
+								we expertise in doing any kind of urbane jobs. We truly comply with all the ISI
+								specifications while designing and manufacturing all our structures which ensure all-out
+								safety and are quite economical as well, as we incorporate quality raw materials and
+								sheets in their making. We also provide our service in Gujarat, Chhattisgarh, Chandigarh
+								& Uttarakhand, Haryana, Uttar Pradesh.</p>
+							<p>Over the last few decades pre-engineered steel and metal buildings have become quite
+								popular in the construction industry due to its flexibility, durability and adaptability
+								over. These factors have made steel one of the most sought after materials for building
+								construction.</p>
+							<p>Our customer has large choice for sheet material which can suit their budget as well as
+								requirement such as pre-coated sheets, Sandwich Panel GI sheets, Fiber sheets, and Ac
+								sheets. In the pre-engineered building systems one has the flexibility to accomplish
+								indefinite array of building configurations, custom designs, requirements and
+								applications. Factory sheds, warehouses, green houses, community halls and canopy etc.,
+								are some of the ways where the PEB systems can be applied.</p>
+							<h4>WE ALSO TAKE SERVICE CONTRACT FOR FOLLOWING WORK :</h4>
+							<p>Maintenance for shed and building structure. Roof Sheet replacement by color coated
+								sheet.</p>
+							<blockquote>STRUCTURAL SYSTEMS FOR PEB</blockquote>
+							<blockquote>PEB COMPOSITION</blockquote>
+							<blockquote>FEATURES THAT MAKES PEB INVALUABLE</blockquote>
+							<p>Our customer has large choice for sheet material which can suit their budget as well as
+								requirement such as Each piece of the PEB system is quite similar; an, I section with
+								end plates for bolting. A crane is used to lift the painted steel sections, and they are
+								then attached together by construction workers who climb to the suitable position. In
+								the construction of huge buildings, we make use of two cranes that work from both ends.
+								When the job of one crane is done, it’s taken away with the other finishing the work.
+								This construction demands the installation of around 6 to 20 bolts that need to be
+								tightened further so as to accurately match the amount of spin using a spin wrench.</p>
+
+							<div class="city-pages-links-container">
+								<div class="row">
+									<blockquote>
+										<a href="pre-engineered-buildings-chandigarh.html" title="Chandigarh"
+											target="_blank">Pre Engineered Building Manufacturers in Chandigarh</a>
+									</blockquote>
+									<blockquote>
+										<a href="pre-engineered-buildings-gujarat.html" title="Gujarat"
+											target="_blank">Pre
+											Engineered Building Manufacturers in Gujarat</a>
+									</blockquote>
+									<blockquote>
+
+										<a href="pre-engineered-buildings-haryana.html" title="Haryana"
+											target="_blank">Pre
+											Engineered Building Manufacturers in Haryana</a>
+									</blockquote>
+									<blockquote>
+
+										<a href="pre-engineered-buildings-uttar-pradesh.html" title="Uttar Pradesh"
+											target="_blank">Pre Engineered Building Manufacturers in Uttar Pradesh</a>
+									</blockquote>
+									<blockquote>
+
+										<a href="pre-engineered-buildings-maharashtra.html">Pre Engineered Building
+											Manufacturers in Maharashtra</a>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<!--Sidebar Start-->
+			<div class="col-lg-3 col-md-12 col-sm-12">
+				<div class="sidebar_wrapper sidebar_wrapper_01">
+					<aside class="widget widget_search">
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Search...">
+							<span class="input-group-btn">
+								<button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+							</span>
+						</div>
+					</aside>
+
+					<aside class="widget widget_categories">
+						<h4 class="widget-title">Categories</h4>
+						<ul>
+							<li><a href="#">taxi trucks</a></li>
+							<li><a href="#">semi trailers</a></li>
+							<li><a href="#">crane trucks</a></li>
+							<li><a href="#">special projects</a></li>
+							<li><a href="#">heavy haulage</a></li>
+							<li><a href="#">wharf transport</a></li>
+							<li><a href="#">freight forwarding</a></li>
+							<li><a href="#">vehicle inspections</a></li>
+							<li><a href="#">warehouse & distribution</a></li>
+						</ul>
+					</aside>
+
+					<aside class="widget widget_archive">
+						<h4 class="widget-title">Archives</h4>
+						<ul>
+							<li><a href="#">january 2022 (8)</a></li>
+							<li><a href="#">february 2022 (6)</a></li>
+							<li><a href="#">December 2022 (5)</a></li>
+							<li><a href="#">October 2022 (3)</a></li>
+							<li><a href="#">May 2022 (8)</a></li>
+							<li><a href="#">August 2022 (7)</a></li>
+						</ul>
+					</aside>
+
+					<aside class="widget widget_calendar">
+						<h4 class="widget-title">calendar</h4>
+						<div class="jquery-calendar"></div>
+					</aside>
+
+					<aside class="widget widget_tag_cloud">
+						<h4 class="widget-title">Search by Tags</h4>
+						<a href="#" class="ed_btn ed_orange">trucks</a>
+						<a href="#" class="ed_btn ed_orange">crane</a>
+						<a href="#" class="ed_btn ed_orange">freight</a>
+						<a href="#" class="ed_btn ed_orange">distribution</a>
+						<a href="#" class="ed_btn ed_orange">transport</a>
+						<a href="#" class="ed_btn ed_orange">heavy</a>
+						<a href="#" class="ed_btn ed_orange">warehouse</a>
+					</aside>
+				</div>
+			</div>
+			<!--Sidebar End-->
+		</div>
+
+	</div>
+</div>
+<!--Blog content end-->
+
+<!--Footer Top section start-->
+<div class="ed_footer_wrapper ed_toppadder60 ed_bottompadder30">
+	<div class="ed_footer_top">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<p><a href="index.html"><img src="assets/images/footer/F_Logo.png" alt="Footer Logo" /></a></p>
+						<p>Transportation is the center of the world! It is the glue of our daily lives. When it goes
+							well, we don't see it. When it goes wrong, it negatively colors our day, makes us feel angry
+							and impotent.</p>
+						<div class="ed_sociallink">
+							<ul>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i
+											class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i
+											class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i
+											class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Linkedin"><i
+											class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Whatsapp"><i
+											class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<h4 class="widget-title">services</h4>
+						<div class="ed_footer_menu">
+							<ul>
+								<li><a href="#">Logistic</a></li>
+								<li><a href="#">Warehousing</a></li>
+								<li><a href="#">Ground Transport</a></li>
+								<li><a href="#">Packaging & Storage</a></li>
+								<li><a href="#">Door-To-Door Delivery</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<h4 class="widget-title">quick links</h4>
+						<div class="ed_footer_menu">
+							<ul>
+								<li><a href="index.html">home</a></li>
+								<li><a href="about.html">about</a></li>
+								<li><a href="gallery.html">gallery</a></li>
+								<li><a href="contact.html">contact us</a></li>
+								<li><a href="private_policy.html">private policy</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<h4 class="widget-title">find us</h4>
+						<p><i class="fa fa-home" aria-hidden="true"></i> 141 N Prospect Ave, Bergenfield, NJ, 07621</p>
+						<p><i class="fa fa-envelope" aria-hidden="true"></i><a href="#"><span class="__cf_email__"
+									data-cfemail="fa93949c95ba8e889b94898a95888e93949d899f888c93999f89d4999597">[email&#160;protected]</span></a>
+							<a href="#"><span class="__cf_email__"
+									data-cfemail="98e8edfaf4f1fbd8eceaf9f6ebe8f7eaecf1f6ffebfdeaeef1fbfdebb6fbf7b6f1f6">[email&#160;protected]</span></a>
+						</p>
+						<p><i class="fa fa-phone" aria-hidden="true"></i> 1800-2202-0909</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Footer Top section end-->
+<!--Footer Bottom section start-->
+<div class="ed_footer_bottom">
+	<div class="container">
+		<div class="col-lg-12 col-md-12 col-sm-12">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12">
+					<div class="ed_copy_right">
+						<p>&copy; Copyright 2022, All Rights Reserved, <a href="#">TRANSPORT</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Footer Bottom section end-->
+
+<!--Page main section end-->
+<!--main js file start-->
+<script data-cfasync="false" src="assets/js/email-decode.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/jquery.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/bootstrap.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/modernizr.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/owl.carousel.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/smooth-scroll.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/jquery.magnific-popup.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/jquery.themepunch.tools.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/jquery.themepunch.revolution.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.actions.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.carousel.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.kenburn.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.layeranimation.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.navigation.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.parallax.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.slideanims.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript"
+	src="assets/js/plugins/revel/revolution.extension.video.min.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/plugins/countto/jquery.countTo.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/plugins/countto/jquery.appear.js"></script>
+<script type="08e575797fa77cbe1666cb90-text/javascript" src="assets/js/custom.js"></script>
+<!--main js file end-->
+<script src="assets/js/rocket-loader.min.js" data-cf-settings="08e575797fa77cbe1666cb90-|49" defer></script>
+</body>
+
+<!-- Mirrored from kamleshyadav.com/html/transport/html/bootstrap5/html/services_single_rs.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Jul 2025 05:07:52 GMT -->
+
+</html>

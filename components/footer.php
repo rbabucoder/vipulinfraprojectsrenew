1f0464af3c7e059679977eb38106f188d08aeb08
unknown <rbabu310197@gmail.com>
2025-07-31 22:29:03 +0530

    add all components of homepage

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,106 @@
+<div class="ed_newsletter_section ed_toppadder90 ed_bottompadder90">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="ed_newsletter_section_heading">
+							<h4>We intend to be the company that best understands and offer services for profitability of our customers globally with expertise and innovation.</h4>
+						</div>
+					</div>
+					<div class="col-lg-12 col-md-12 col-sm-12 col-12 d-flex flex-wrap justify-content-center gap-2">
+						<button class="btn ed_btn ed_green">Get a Quote</button>
+						<a href="tel:+91-9811178465" class="btn ed_btn ed_green"> +91-9811178465</a>
+					</div>
+					<!-- <div class="col-lg-3"></div>
+					<div class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
+						<form>
+							<div class="ed_newsletter_section_form">
+								<input class="form-control" type="text" placeholder="Newsletter Email" />
+							</div>
+						</form>
+					</div>
+					<div class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
+						<form>
+							<button class="btn ed_btn ed_green">confirm</button>
+						</form>
+					</div> -->
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="ed_footer_wrapper ed_toppadder60 ed_bottompadder30">
+	<div class="ed_footer_top">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<p><a href="index.html"><img src="assets/images/footer/F_Logo.png" alt="Footer Logo" /></a></p>
+						<p>Transportation is the center of the world! It is the glue of our daily lives. When it goes
+							well, we don't see it. When it goes wrong, it negatively colors our day, makes us feel angry
+							and impotent.</p>
+						<div class="ed_sociallink">
+							<ul>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i
+											class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i
+											class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i
+											class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Linkedin"><i
+											class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Whatsapp"><i
+											class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<h4 class="widget-title">services</h4>
+						<div class="ed_footer_menu">
+							<ul>
+								<li><a href="#">Logistic</a></li>
+								<li><a href="#">Warehousing</a></li>
+								<li><a href="#">Ground Transport</a></li>
+								<li><a href="#">Packaging & Storage</a></li>
+								<li><a href="#">Door-To-Door Delivery</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<h4 class="widget-title">quick links</h4>
+						<div class="ed_footer_menu">
+							<ul>
+								<li><a href="index.html">home</a></li>
+								<li><a href="about.html">about</a></li>
+								<li><a href="gallery.html">gallery</a></li>
+								<li><a href="contact.html">contact us</a></li>
+								<li><a href="private_policy.html">private policy</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-12">
+					<div class="widget text-widget">
+						<h4 class="widget-title">find us</h4>
+						<p><i class="fa fa-home" aria-hidden="true"></i> 141 N Prospect Ave, Bergenfield, NJ, 07621</p>
+						<p><i class="fa fa-envelope" aria-hidden="true"></i><a href="#"><span class="__cf_email__"
+									data-cfemail="442d2a222b043036252a37342b36302d2a23372136322d2721376a272b29">[email&#160;protected]</span></a>
+							<a href="#"><span class="__cf_email__"
+									data-cfemail="c8b8bdaaa4a1ab88bcbaa9a6bbb8a7babca1a6afbbadbabea1abadbbe6aba7e6a1a6">[email&#160;protected]</span></a>
+						</p>
+						<p><i class="fa fa-phone" aria-hidden="true"></i> 1800-2202-0909</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <!--Footer Bottom section start-->
 <div class="ed_footer_bottom">
 	<div class="container">
--- project.php
+++ project.php
@@ -115,36 +115,7 @@
     </div><!-- /.container -->
 </div>
 <!-- gallery section end -->
-<!--Newsletter Section six start-->
-<div class="ed_newsletter_section ed_toppadder90 ed_bottompadder90">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 col-md-12 col-sm-12">
-				<div class="row">
-					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-						<div class="ed_newsletter_section_heading">
-							<h4>Let us inform you about everything important directly.</h4>
-						</div>
-					</div>
-					<div class="col-lg-3"></div>
-	<div class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
-		<form>
-			<div class="ed_newsletter_section_form">
-				<input class="form-control" type="text" placeholder="Newsletter Email" />
-			</div>
-		</form>
-	</div>
-	<div class="col-lg-4 col-md-6 col-sm-6 col-xs-6">
-		<form>
-			<button class="btn ed_btn ed_green">confirm</button>
-		</form>
-	</div>
-				</div>
-			</div>
-        </div>
-	</div>
-</div>
-<!--Newsletter Section six end-->
+
 <!--Footer Top section start-->
 <div class="ed_footer_wrapper ed_toppadder60 ed_bottompadder30">
 	<div class="ed_footer_top">

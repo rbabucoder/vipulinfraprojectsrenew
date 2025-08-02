20f848c2e95233103606f9d0dda3ffb6f334b2db
rbabucoder <baburohit0131@gmail.com>
2025-08-02 11:36:19 +0530

    update header menu

--- components/header.php
+++ components/header.php
@@ -15,71 +15,98 @@ Purchase: http://themeforest.net/user/kamleshyadav
 <!-- Begin Head -->
 
 <!-- Mirrored from kamleshyadav.com/html/transport/html/bootstrap5/html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Jul 2025 05:07:07 GMT -->
-<head>
-<meta charset="utf-8" />
-<title>Transporting Multipurpose Responsive HTML Template</title>
-<meta content="width=device-width, initial-scale=1.0" name="viewport" />
-<meta name="description"  content="Transporting"/>
-<meta name="keywords" content="Transporting, html template" />
-<meta name="author"  content="Kamleshyadav"/>
-<meta name="MobileOptimized" content="320" />
 
-<!--srart theme style -->
-<link href="assets/css/main.css" rel="stylesheet" type="text/css"/>
-<!-- end theme style -->
-<!-- favicon links -->
-<link rel="shortcut icon" type="image/png" href="assets/images/header/favicon.png" />
+<head>
+	<meta charset="utf-8" />
+	<title>Transporting Multipurpose Responsive HTML Template</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<meta name="description" content="Transporting" />
+	<meta name="keywords" content="Transporting, html template" />
+	<meta name="author" content="Kamleshyadav" />
+	<meta name="MobileOptimized" content="320" />
+
+	<!--srart theme style -->
+	<link href="assets/css/main.css" rel="stylesheet" type="text/css" />
+	<!-- end theme style -->
+	<!-- favicon links -->
+	<link rel="shortcut icon" type="image/png" href="assets/images/header/favicon.png" />
 </head>
+
 <body>
-<!--Page main section start-->
-<div id="pro_wrapper">
-<!--Header start-->
-<header id="ed_header_wrapper" class="header_transparent">
-	<div class="ed_header_top">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-					<div class="pro_call">
-						<p>call now <i class="fa fa-volume-control-phone" aria-hidden="true"></i> 1800-2202-0909</p>
-					</div>
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-					<div class="ed_info_wrapper">
-						<ul>
-							<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-							<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-							<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-							<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-							<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Whatsapp"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
-						</ul>
+	<!--Page main section start-->
+	<div id="pro_wrapper">
+		<!--Header start-->
+		<header id="ed_header_wrapper" class="header_transparent">
+			<div class="ed_header_top">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<div class="pro_call">
+								<p>call now <i class="fa fa-volume-control-phone" aria-hidden="true"></i> 1800-2202-0909
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<div class="ed_info_wrapper">
+								<ul>
+									<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i
+												class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i
+												class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+									<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i
+												class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Linkedin"><i
+												class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Whatsapp"><i
+												class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-	<div class="ed_header_bottom">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3 col-md-3 col-sm-4">
-					<div class="pro_logo"> <a href="/"><img src="assets/images/header/Logo_yellow.png" alt="logo" /></a> </div>
-				</div>
-				<div class="col-lg-9 col-md-9 col-sm-9">
-					<div class="pro_menu">
-						<ul>
-							<li class="has_submenu"><a href="#">Home <i class="fa fa-angle-down" aria-hidden="true"></i></a>
-								<ul class="sub-menu sub-menu1">
-									<li><a href="index.html">Home v1</a></li>
-									<li><a href="index_2.html">home v2</a></li>
-									<li><a href="index_3.html">home v3</a></li>
-									<li><a href="index_4.html">home v4</a></li>
-									<li><a href="index_5.html">home v5</a></li>
-									<li><a href="index_6.html">home v6</a></li>
-									<li><a href="../transport_final/Transport_html/index_7.html">home v7</a></li>
-									<li><a href="https://kamleshyadav.com/html/transport/html/bootstrap5/index7/index_7.html">home v7</a></li>
-								</ul>
-							</li>
-							<li><a href="about.php">about us</a></li>
-							<li class="has_submenu"><a href="javascript:;">Services <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+			<div class="ed_header_bottom">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 col-md-3 col-sm-4">
+							<div class="pro_logo"> <a href="/"><img src="assets/images/header/Logo_yellow.png"
+										alt="logo" /></a> </div>
+						</div>
+						<div class="col-lg-9 col-md-9 col-sm-9">
+							<div class="pro_menu">
+								<ul>
+									<!-- <li class="has_submenu"><a href="#">Home 
+										<i class="fa fa-angle-down"
+												aria-hidden="true"></i></a>
+										<ul class="sub-menu sub-menu1">
+											<li><a href="index.html">Home v1</a></li>
+											<li><a href="index_2.html">home v2</a></li>
+											<li><a href="index_3.html">home v3</a></li>
+											<li><a href="index_4.html">home v4</a></li>
+											<li><a href="index_5.html">home v5</a></li>
+											<li><a href="index_6.html">home v6</a></li>
+											<li><a href="../transport_final/Transport_html/index_7.html">home v7</a>
+											</li>
+											<li><a
+													href="https://kamleshyadav.com/html/transport/html/bootstrap5/index7/index_7.html">home
+													v7</a></li>
+										</ul>
+									</li> -->
+									<li><a href="/">Home</a></li>
+									<li><a href="about.php">about us</a></li>
+									<li class="has_submenu"><a href="javascript:;">Products <i class="fa fa-angle-down"
+												aria-hidden="true"></i></a>
+										<ul class="sub-menu sub-menu1">
+											<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/pre-engineered-buildings.php">Pre Engineered Building</a></li>
+											<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/warehouse.php">PEB Warehouse</a></li>
+											<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/sheet-accesories.php">Sheet & Accessories </a></li>
+											<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/z-and-c-purlin.php">Purlin</a></li>
+											<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/prefabricated-steel-buildings-structures.php">Prefabricates Steel Buildings</a></li>
+											<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/industrial-shed-manufacturers.php">Industrial Sheds</a></li>
+											<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/color-coated-sheets.php">Color Coated Sheets</a></li>
+										</ul>
+									</li>
+									<!-- <li class="has_submenu"><a href="javascript:;">Services <i class="fa fa-angle-down" aria-hidden="true"></i></a>
 								<ul class="sub-menu sub-menu1">
 									<li><a href="#">services</a>
 										
@@ -94,8 +121,8 @@ Purchase: http://themeforest.net/user/kamleshyadav
 										</ul>
 									</li>
 								</ul>
-							</li>
-							<li  class="has_submenu"><a href="#">blog  <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+							</li> -->
+									<!-- <li  class="has_submenu"><a href="#">blog  <i class="fa fa-angle-down" aria-hidden="true"></i></a>
 								<ul class="sub-menu sub-menu1">
 									<li><a href="#">blog large</a></li>
 									<li><a href="#">blog medium </a></li>
@@ -109,15 +136,23 @@ Purchase: http://themeforest.net/user/kamleshyadav
 										</ul>
 									</li>
 								</ul>
-							</li>
-							<li><a href="gallery.html">gallery</a></li>
-							<li><a href="#">Contact Us <i class="fa fa-angle-down" aria-hidden="true"></i></a>
-								<ul class="sub-menu sub-menu1">
-									<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/enquiry.php">Enquiry</a></li>
-									<li><a href="https://www.vipulinfraprojects.com/assets/images/COMPANY%20PROFILE(VIP).pdf">E-Catalogue </a></li>
-									<li><a href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/careers.php">Careers</a></li>
-								</ul>
-							<!-- <li><a href="#">Pages <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+							</li> -->
+									<li><a
+											href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/project.php">Project</a>
+									</li>
+									<li><a href="#">Contact Us <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+										<ul class="sub-menu sub-menu1">
+											<li><a
+													href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/enquiry.php">Enquiry</a>
+											</li>
+											<li><a
+													href="https://www.vipulinfraprojects.com/assets/images/COMPANY%20PROFILE(VIP).pdf">E-Catalogue
+												</a></li>
+											<li><a
+													href="https://www.vipulinfraprojects.com/vipulinfraprojectsrenew/careers.php">Careers</a>
+											</li>
+										</ul>
+										<!-- <li><a href="#">Pages <i class="fa fa-angle-down" aria-hidden="true"></i></a>
 								<ul class="sub-menu sub-menu1">
 									<li><a href="team.html">Team</a></li>
 									<li><a href="pricing_table.html">pricing table</a></li>
@@ -126,16 +161,16 @@ Purchase: http://themeforest.net/user/kamleshyadav
 									<li><a href="faq.html">FAQ</a></li>
 									<li><a href="coming_soon.html">coming soon</a></li>
 								</ul> -->
-							</li>
-							<li><a href="contact.html">Contact</a></li>
-						</ul>
-							<div class="ed_toggle_bar">
-							<i class="fa fa-bars" aria-hidden="true"></i>
+									</li>
+									<!-- <li><a href="contact.html">Contact</a></li> -->
+								</ul>
+								<div class="ed_toggle_bar">
+									<i class="fa fa-bars" aria-hidden="true"></i>
+								</div>
+
+							</div>
 						</div>
-						
 					</div>
 				</div>
 			</div>
-		</div>
-    </div>
-</header>
+		</header>
